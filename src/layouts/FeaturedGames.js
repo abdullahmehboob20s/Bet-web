58c164b5908ee43d80a3a465875a6edf5155d8d2
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-07 12:23:17 +0500

    created

---
+++ src/layouts/FeaturedGames.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function FeaturedGames() {
+  return (
+    <div>
+      <div className="container">
+        <h1 className="heading mb-5px">Featured Games</h1>
+        <div className="py-[5vh] px-10px">
+          <p className="text-white-7 text-center text-13px font-medium">
+            The are no featured games at the moment
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FeaturedGames;
