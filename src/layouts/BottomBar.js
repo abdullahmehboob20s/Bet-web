b92908ae401ee2660e8d1c744fc62e3caf65cf22
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-07 16:49:31 +0500

    fix lag issue

--- src/layouts/BottomBar.js
+++ src/layouts/BottomBar.js
@@ -3,7 +3,7 @@ import { FiMenu } from "react-icons/fi";
 
 function BottomBar() {
   return (
-    <div className="fixed left-0 bg-[rgba(0,12,36,.9)] border-t-1px border-white-2 bottom-0 w-full h-mobile-header-main-section-height z-[100] backdrop-blur-[3px] flex items-center">
+    <div className="fix-lag fixed left-0 bg-[rgba(0,12,36,.9)] border-t-1px border-white-2 bottom-0 w-full h-mobile-header-main-section-height z-[100] backdrop-blur-[3px] flex items-center">
       <BottomBarLink title="Live" />
       <BottomBarLink title="Sports" />
       <BottomBarLink title="BestSlip" />
