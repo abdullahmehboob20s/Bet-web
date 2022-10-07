64be18445a3396608486ed986139d3e47f41ad4f
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-07 12:07:29 +0500

    created

---
+++ src/layouts/Promotion.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Promotion() {
+  return (
+    <div>
+      <div className="container">
+        <a href="/" className="rounded overflow-hidden block">
+          <img src="images/promotion-banner.webp" className="w-full" alt="" />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Promotion;
