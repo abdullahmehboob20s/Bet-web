0902b25cd8f8bc3e1a9c36c74eb27393daec3660
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-07 12:56:36 +0500

    added sections

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import Banner from "layouts/Banner";
+import CasinoGames from "layouts/CasinoGames";
 import FeaturedGames from "layouts/FeaturedGames";
+import Games from "layouts/Games";
 import Jackpot from "layouts/Jackpot";
+import LiveCasino from "layouts/LiveCasino";
 import Navbar from "layouts/Navbar";
 import Promotion from "layouts/Promotion";
 
 function App() {
   return (
-    <div>
+    <div className="pb-20">
       <Navbar />
       <div className="mt-mobile-header-full-height space-y-10px">
         <Banner />
         <Jackpot />
         <Promotion />
         <FeaturedGames />
+        <div className="space-y-6">
+          <CasinoGames />
+          <LiveCasino />
+          <Games />
+        </div>
       </div>
     </div>
   );
