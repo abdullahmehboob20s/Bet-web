75dc368c632fd9143895c5e58bd0f2b0dacf6589
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-07 12:08:53 +0500

    added sctions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Banner from "layouts/Banner";
+import Jackpot from "layouts/Jackpot";
 import Navbar from "layouts/Navbar";
+import Promotion from "layouts/Promotion";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className="mt-mobile-header-full-height">
+      <div className="mt-mobile-header-full-height space-y-10px">
         <Banner />
+        <Jackpot />
+        <Promotion />
       </div>
     </div>
   );
