c6d9075afc10fc11cf338efc8b8172ba110c05c0
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-10 12:21:21 +0500

    added options and passing props

--- src/layouts/EventView/FilterBar.js
+++ src/layouts/EventView/FilterBar.js
@@ -1,32 +1,58 @@
 import FilterButton from "components/FilterButton";
 import React from "react";
-import { FaVideo } from "react-icons/fa";
+import { FaTrophy, FaVideo } from "react-icons/fa";
 import { BiGlobe } from "react-icons/bi";
 import { AiFillCloud } from "react-icons/ai";
 import SearchBar from "components/SearchBar";
+import SelectBox from "components/SelectBox";
+
+const options = [
+  { title: "Select Tournaments", value: "" },
+  { title: "Русский", value: "" },
+  { title: "ქართული", value: "" },
+  { title: "Français", value: "" },
+  { title: "Հայերեն", value: "" },
+];
 
 function FilterBar() {
   return (
-    <div className="container-wrapper flex items-center justify-between relative">
-      <div className="flex items-center space-x-10px">
-        <FilterButton
-          Icon={({ className }) => (
-            <FaVideo className={`text-sm ${className}`} />
-          )}
-        />
-        <FilterButton
-          Icon={({ className }) => (
-            <BiGlobe className={`text-base ${className}`} />
-          )}
-        />
-        <FilterButton
-          Icon={({ className }) => (
-            <AiFillCloud className={`text-base ${className}`} />
-          )}
-        />
+    <div>
+      <div className="container-wrapper flex items-center justify-between relative mb-10px">
+        <div className="flex items-center space-x-10px">
+          <FilterButton
+            Icon={({ className }) => (
+              <FaVideo className={`text-sm ${className}`} />
+            )}
+          />
+          <FilterButton
+            Icon={({ className }) => (
+              <BiGlobe className={`text-base ${className}`} />
+            )}
+          />
+          <FilterButton
+            Icon={({ className }) => (
+              <AiFillCloud className={`text-base ${className}`} />
+            )}
+          />
+        </div>
+
+        <SearchBar />
       </div>
 
-      <SearchBar />
+      <div className="container-wrapper">
+        <SelectBox
+          title="TOURNAMENTS"
+          options={options}
+          showDropdownIcon={false}
+          bg="bg-blue-to-purple"
+          height="h-[40px]"
+          paddingTop="pt-4"
+          titleClassName="absolute top-[3px] text-[10px] text-white-5"
+          fromLeft="pl-[38px]"
+        >
+          <FaTrophy className="absolute top-1/2 -translate-y-1/2 left-3 text-base fill-white-8" />
+        </SelectBox>
+      </div>
     </div>
   );
 }
