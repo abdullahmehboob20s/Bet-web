de946a3039a4b63b32855574c603ad1cf6818026
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-10 10:04:24 +0500

    created options list and passing them

--- src/layouts/Dropdowns.js
+++ src/layouts/Dropdowns.js
@@ -1,12 +1,20 @@
 import SelectBox from "components/SelectBox";
 import React from "react";
 
+const options = [
+  { title: "English", value: "" },
+  { title: "Русский", value: "" },
+  { title: "ქართული", value: "" },
+  { title: "Français", value: "" },
+  { title: "Հայերեն", value: "" },
+];
+
 function Dropdowns() {
   return (
     <div>
       <div className="container-wrapper grid grid-cols-2 gap-2">
-        <SelectBox />
-        <SelectBox />
+        <SelectBox options={options} />
+        <SelectBox options={options} />
       </div>
     </div>
   );
