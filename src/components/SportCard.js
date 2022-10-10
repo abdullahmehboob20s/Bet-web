70f83fa39af8a837cd1a403162bd21bd08e81136
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-10 17:39:45 +0500

    implemented a sticky functionality when selected

--- src/components/SportCard.js
+++ src/components/SportCard.js
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 
-function SportCard({ id, title, Icon, color = "rgba(255,255,255,.1)" }) {
-  const [active, setActive] = useState(false);
-
-  const toggle = () => setActive((val) => !val);
-
+function SportCard({
+  id,
+  title,
+  Icon,
+  isSelected,
+  color = "rgba(255,255,255,.1)",
+  onClick,
+}) {
   return (
     <button
-      onClick={toggle}
-      className={`outline-none p-5px rounded text-white-5 relative flex flex-col min-w-[80px] h-[60px] text-center items-center justify-center`}
-      style={{ backgroundColor: active ? color : "rgba(255,255,255,.1)" }}
+      onClick={() => onClick(id)}
+      className={`outline-none p-5px rounded text-white-5 relative flex flex-col min-w-[80px] h-[60px] text-center items-center justify-center ${
+        isSelected ? "sticky left-0 right-0 z-50" : ""
+      }`}
+      style={{ backgroundColor: isSelected ? color : "rgba(255,255,255,.1)" }}
     >
       <span className="h-[38px] flex items-center justify-center">
-        {Icon && <Icon style={{ fill: active ? "white" : color }} />}
+        {Icon && <Icon style={{ fill: isSelected ? "white" : color }} />}
       </span>
       <span
-        className={`text-10px block ${active ? "text-white" : "text-white-5"}`}
+        className={`text-10px block ${
+          isSelected ? "text-white" : "text-white-5"
+        }`}
       >
         {title}
       </span>
