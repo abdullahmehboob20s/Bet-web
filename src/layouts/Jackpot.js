3cd055769800c04314356481c0b5e0bb87879060
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-10 10:04:54 +0500

    made separat component of wrapper

--- src/layouts/Jackpot.js
+++ src/layouts/Jackpot.js
@@ -3,51 +3,48 @@ import Slider from "components/Slider";
 import React, { useRef } from "react";
 import { SwiperSlide } from "swiper/react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import JackPotWrapper from "components/JackPotWrapper";
 
 function Jackpot() {
   const prevRef = useRef(null);
   const nextRef = useRef(null);
   return (
-    <div>
-      <div className="container-wrapper">
-        <div className="py-18px rounded jackpot-banner z-10 relative">
-          <Slider
-            slidesPerView={1}
-            options={{
-              loop: true,
-            }}
-            prevRef={prevRef}
-            nextRef={nextRef}
-          >
-            <SwiperSlide>
-              <JackpotCard />
-            </SwiperSlide>
-            <SwiperSlide>
-              <JackpotCard />
-            </SwiperSlide>
-            <SwiperSlide>
-              <JackpotCard />
-            </SwiperSlide>
-            <SwiperSlide>
-              <JackpotCard />
-            </SwiperSlide>
-          </Slider>
+    <JackPotWrapper>
+      <Slider
+        slidesPerView={1}
+        options={{
+          loop: true,
+        }}
+        prevRef={prevRef}
+        nextRef={nextRef}
+      >
+        <SwiperSlide>
+          <JackpotCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <JackpotCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <JackpotCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <JackpotCard />
+        </SwiperSlide>
+      </Slider>
 
-          <button
-            ref={prevRef}
-            className="absolute top-1/2 -translate-y-1/2 left-0 z-50 text-2xl"
-          >
-            <IoIosArrowBack />
-          </button>
-          <button
-            ref={nextRef}
-            className="absolute top-1/2 -translate-y-1/2 right-0 z-50 text-2xl"
-          >
-            <IoIosArrowForward />
-          </button>
-        </div>
-      </div>
-    </div>
+      <button
+        ref={prevRef}
+        className="absolute top-1/2 -translate-y-1/2 left-0 z-50 text-2xl"
+      >
+        <IoIosArrowBack />
+      </button>
+      <button
+        ref={nextRef}
+        className="absolute top-1/2 -translate-y-1/2 right-0 z-50 text-2xl"
+      >
+        <IoIosArrowForward />
+      </button>
+    </JackPotWrapper>
   );
 }
 
