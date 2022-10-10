cacc350a6e473695a8fc28163a3ff9683b13b4ad
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-10 10:04:04 +0500

    replaced static options with dynamic options

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -1,35 +1,24 @@
 import { IoIosArrowDown } from "react-icons/io";
 
-function SelectBox() {
+function SelectBox({ options, showDropdownIcon = true }) {
   return (
     <label className="h-[52px] flex items-center rounded overflow-hidden relative cursor-pointer bg-[rgba(255,255,255,0.15)]">
       <select className="w-full h-full outline-none flex-col appearance-none text-white-9 cursor-pointer text-sm border-none pb-0 pt-6 px-[15px] bg-transparent">
-        <option className="bg-b" value="eng">
-          English
-        </option>
-        <option className="bg-b" value="rus">
-          Русский
-        </option>
-        <option className="bg-b" value="geo">
-          ქართული
-        </option>
-        <option className="bg-b" value="fra">
-          Français
-        </option>
-        <option className="bg-b" value="arm">
-          Հայերեն
-        </option>
-        <option className="bg-b" value="arb">
-          العربية
-        </option>
+        {options.map((item, index) => (
+          <option className="bg-b" value={item.value}>
+            {item.title}
+          </option>
+        ))}
       </select>
       <span className="pointer-events-none select-none text-white-4 absolute top-[8px] left-[15px] text-xs">
         Language
       </span>
 
-      <button className="absolute top-1/2 -translate-y-1/2 right-5 text-base">
-        <IoIosArrowDown />
-      </button>
+      {showDropdownIcon && (
+        <button className="absolute pointer-events-none top-1/2 -translate-y-1/2 right-5 text-base">
+          <IoIosArrowDown />
+        </button>
+      )}
     </label>
   );
 }
