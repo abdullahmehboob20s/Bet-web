1a80db58918cf255433e97d4dfdc6debf9597b46
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-10 08:22:58 +0500

    created links data

---
+++ src/assets/data/live-page-links-data.js
@@ -0,0 +1,7 @@
+const livePageLinksData = [
+  { title: "EVENT VIEW", link: "event-view", target: "_self" },
+  { title: "live calendar", link: "live-calendar", target: "_self" },
+  { title: "statistics", link: "statistics", target: "_self" },
+];
+
+export default livePageLinksData;
