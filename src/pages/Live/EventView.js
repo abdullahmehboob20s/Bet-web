e67458ef170887729a5060d6c5d0656e82de4bde
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-10 10:05:17 +0500

    added sections

--- src/pages/Live/EventView.js
+++ src/pages/Live/EventView.js
@@ -1,9 +1,19 @@
+import JackpotCard from "components/JackpotCard";
+import JackPotWrapper from "components/JackPotWrapper";
+import FilterBar from "layouts/EventView/FilterBar";
 import React from "react";
 
 function EventView() {
   return (
     <div>
-      <h1>EventView</h1>
+      <div className="mb-4">
+        <JackPotWrapper>
+          <JackpotCard />
+        </JackPotWrapper>
+      </div>
+      <div className="mb-10px">
+        <FilterBar />
+      </div>
     </div>
   );
 }
