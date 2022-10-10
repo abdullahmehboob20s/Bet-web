3fb0faebd8e8f49a5fd830f74b75959a5e794cb7
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-10 07:25:01 +0500

    created data for links

---
+++ src/assets/data/navbar-links-data.js
@@ -0,0 +1,17 @@
+const navbarLinksData = [
+  { title: "Live", isNew: false, link: "/live", target: "_self" },
+  { title: "Sports", isNew: false, link: "/", target: "_self" },
+  { title: "Pinnacle", isNew: false, link: "/", target: "_self" },
+  { title: "Casino", isNew: false, link: "/", target: "_self" },
+  { title: "Live Casino", isNew: false, link: "/", target: "_self" },
+  { title: "Sport Tournaments", isNew: true, link: "/", target: "_self" },
+  { title: "Poker", isNew: false, link: "/", target: "_self" },
+  { title: "Predictor", isNew: true, link: "/", target: "_self" },
+  { title: "Esports", isNew: false, link: "/", target: "_self" },
+  { title: "Virtual Sports", isNew: false, link: "/", target: "_self" },
+  { title: "Games", isNew: false, link: "/", target: "_self" },
+  { title: "TV Games", isNew: false, link: "/", target: "_self" },
+  { title: "Promotions", isNew: false, link: "/", target: "_self" },
+];
+
+export default navbarLinksData;
