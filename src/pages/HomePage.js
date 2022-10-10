3e105d93dda64bd11f69fdf4610fc25e36977679
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-10 07:27:18 +0500

    created a home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,35 @@
+import NavbarLinks from "components/NavbarLinks";
+import Banner from "layouts/Banner";
+import CasinoGames from "layouts/CasinoGames";
+import FeaturedGames from "layouts/FeaturedGames";
+import Games from "layouts/Games";
+import Jackpot from "layouts/Jackpot";
+import Layout from "layouts/Layout";
+import LiveCasino from "layouts/LiveCasino";
+import Promotion from "layouts/Promotion";
+
+function HomePage() {
+  return (
+    <Layout navbarProps={{ NavbarBottomContent: NavbarLinks }}>
+      <div className="mb-10px">
+        <Banner />
+      </div>
+      <div className="mb-10px">
+        <Jackpot />
+      </div>
+      <div className="mb-10px">
+        <Promotion />
+      </div>
+      <div className="mb-10px">
+        <FeaturedGames />
+      </div>
+      <div className="space-y-6 mb-6">
+        <CasinoGames />
+        <LiveCasino />
+        <Games />
+      </div>
+    </Layout>
+  );
+}
+
+export default HomePage;
