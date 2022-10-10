1ce73a967900dfe32d4ec926a8061c1d4fe38f3f
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-10 17:40:06 +0500

    added a dynamic data

--- src/layouts/SportsCardList.js
+++ src/layouts/SportsCardList.js
@@ -1,43 +1,78 @@
 import SportCard from "components/SportCard";
-import React from "react";
+import React, { useState } from "react";
 import { FaTableTennis, FaVolleyballBall } from "react-icons/fa";
 import { IoIosTennisball, IoMdBasketball } from "react-icons/io";
 import { IoFootball } from "react-icons/io5";
 
 function SportsCardList() {
+  const [data, setData] = useState([
+    {
+      id: "7",
+      color: "#109121",
+      title: "Football",
+      Icon: IoFootball,
+      iconSize: "text-4xl",
+      isSelected: false,
+    },
+    {
+      id: "4",
+      color: "#fca601",
+      title: "Basketball",
+      Icon: IoMdBasketball,
+      iconSize: "text-4xl",
+      isSelected: false,
+    },
+    {
+      id: "12",
+      title: "Tennis",
+      color: "#a19e3e",
+      Icon: IoIosTennisball,
+      iconSize: "text-4xl",
+      isSelected: false,
+    },
+    {
+      id: "14",
+      title: "Table Tenis",
+      color: "#839d24",
+      Icon: FaTableTennis,
+      iconSize: "text-2xl",
+      isSelected: false,
+    },
+    {
+      id: "8",
+      title: "Volleyball",
+      color: "#dac179",
+      Icon: FaVolleyballBall,
+      iconSize: "text-2xl",
+      isSelected: false,
+    },
+  ]);
+
+  const changeSelected = (id) => {
+    const newData = data.map((item) => {
+      if (item.id == id) {
+        return { ...item, isSelected: true };
+      } else {
+        return { ...item, isSelected: false };
+      }
+    });
+
+    setData(newData);
+  };
+
   return (
     <div>
       <div className="container-wrapper flex overflow-x-scroll hide-scrollbar space-x-2">
-        <SportCard
-          id="7"
-          color="#109121"
-          title="Football"
-          Icon={(props) => <IoFootball className="text-4xl" {...props} />}
-        />
-        <SportCard
-          id="4"
-          color="#fca601"
-          title="Basketball"
-          Icon={(props) => <IoMdBasketball className="text-4xl" {...props} />}
-        />
-        <SportCard
-          id="12"
-          title="Tennis"
-          color="#a19e3e"
-          Icon={(props) => <IoIosTennisball className="text-4xl" {...props} />}
-        />
-        <SportCard
-          id="14"
-          title="Table Tenis"
-          color="#839d24"
-          Icon={(props) => <FaTableTennis className="text-2xl" {...props} />}
-        />
-        <SportCard
-          id="8"
-          title="Volleyball"
-          color="#dac179"
-          Icon={(props) => <FaVolleyballBall className="text-3xl" {...props} />}
-        />
+        {data.map((item, index) => (
+          <SportCard
+            id={item.id}
+            color={item.color}
+            title={item.title}
+            onClick={changeSelected}
+            isSelected={item.isSelected}
+            Icon={(props) => <item.Icon className={item.iconSize} {...props} />}
+          />
+        ))}
       </div>
     </div>
   );
