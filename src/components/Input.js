bc51470214e76faf391b90a8ac53593788dc5c36
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-12 21:10:09 +0500

    added value logic

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 const Input = ({ label, name, type = "text", ...rest }) => {
+  const [typeState, setTypeState] = useState(type);
+  const [value, setValue] = useState("");
+
+  const togglePasswordType = () => {
+    if (type === "password") {
+      setTypeState((val) => (val === "password" ? "text" : "password"));
+    }
+  };
+
   return (
     <div className="relative z-0">
       <input
         {...rest}
         id={name}
-        type={type}
+        type={typeState}
         className="floating-label w-full appearance-none focus:outline-none pl-[15px] text-white-9 flex flex-col h-[52px] bg-[rgba(255,255,255,0.15)] rounded pr-[44px] text-sm pt-4"
         placeholder=" "
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
       <label
         htmlFor={name}
@@ -17,6 +29,26 @@ const Input = ({ label, name, type = "text", ...rest }) => {
       >
         {label}
       </label>
+
+      {typeState === "password" && type === "password" && value !== "" ? (
+        <button
+          onClick={togglePasswordType}
+          type="button"
+          className="absolute right-4 vertical-position-center z-20 text-2xl"
+        >
+          <AiFillEyeInvisible className="fill-white-7" />
+        </button>
+      ) : null}
+
+      {type === "password" && typeState === "text" && value !== "" ? (
+        <button
+          onClick={togglePasswordType}
+          type="button"
+          className="absolute right-4 vertical-position-center z-20 text-2xl"
+        >
+          <AiFillEye className="fill-white-7" />
+        </button>
+      ) : null}
     </div>
   );
 };
