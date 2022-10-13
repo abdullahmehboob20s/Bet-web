a9096e914cb522ae5a6788ad9fed7e76e770848f
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-13 10:14:38 +0500

    added conditional rendering

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,20 +1,21 @@
 import React from "react";
 
 function Checkbox(props) {
+  const { className = "flex items-center space-x-3", inputClassName } = props;
   return (
-    <div className="checkbox flex items-center space-x-3">
+    <div className={`checkbox ${className}`}>
       <input
         type={"checkbox"}
         name={props.name}
         id={props.id}
-        className="w-4 h-4 rounded bg-white-2 checked:bg-hero"
+        className={`min-w-[16px] h-4 rounded bg-white-2 checked:bg-hero ${inputClassName}`}
       />
       {props.title && (
         <label
           htmlFor={props.id}
           className="text-sm text-white-7 cursor-pointer"
         >
-          {props.title}
+          {props.title && props.title}
         </label>
       )}
     </div>
