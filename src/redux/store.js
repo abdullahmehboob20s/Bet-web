e9cb5f097dd8f8441143005cfc3c77eb42f8961b
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-13 10:15:34 +0500

    added register modal slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import registerModalState from "./registerModalState";
 import signInModalState from "./signInModalState";
 
 export const store = configureStore({
   reducer: {
     signInModalState,
+    registerModalState,
   },
 });
