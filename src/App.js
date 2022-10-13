9b45dafaeb1dbdf57298dbb6519a6fafb5889279
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-13 10:14:18 +0500

    added register modal

--- src/App.js
+++ src/App.js
@@ -11,55 +11,68 @@ import FAQ from "pages/FAQ";
 import SignInModal from "layouts/SignInModal";
 import { useSelector } from "react-redux";
 import useDelayUnmount from "hooks/useDelayUnmount";
+import RegisterModal from "layouts/RegisterModal";
 
 function App() {
   const location = useLocation();
   const background = location.state && location.state.background;
   const { isOpen } = useSelector((state) => state.signInModalState);
-  const shouldSignModalRender = useDelayUnmount(isOpen, 1000);
+  const { isRegisterModalOpen } = useSelector(
+    (state) => state.registerModalState
+  );
+  const shouldSignModalRender = useDelayUnmount(isOpen, 300);
+  const shouldRegisterModalRender = useDelayUnmount(isRegisterModalOpen, 300);
 
   useEffect(() => {
     if (
       location.pathname === "/privacy-policy" ||
       location.pathname === "/faq" ||
-      isOpen
+      isOpen ||
+      isRegisterModalOpen
     ) {
       document.body.style.overflowY = "hidden";
     } else {
       document.body.style.overflowY = "scroll";
     }
-  }, [location.pathname, isOpen]);
+  }, [location.pathname, isRegisterModalOpen, isOpen]);
 
   return (
-    <div>
+    <>
+      {shouldRegisterModalRender && (
+        <RegisterModal
+          className={isRegisterModalOpen ? "fade-in" : "fade-out"}
+        />
+      )}
+
       {shouldSignModalRender && (
         <SignInModal className={isOpen ? "fade-in" : "fade-out"} />
       )}
+      <div>
+        <Routes location={background || location}>
+          <Route index element={<HomePage />} />
+          <Route path="sports" element={<Sports />} />
+          <Route path="casino" element={<Casino />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="live" element={<Live />}>
+            <Route index element={<EventView />} />
+            <Route path="event-view" element={<EventView />} />
+            <Route path="*" element={<Navigate to="event-view" replace />} />
+          </Route>
+        </Routes>
 
-      <Routes location={background || location}>
-        <Route index element={<HomePage />} />
-        <Route path="sports" element={<Sports />} />
-        <Route path="casino" element={<Casino />}>
-          <Route index element={<Home />} />
-        </Route>
-        <Route path="live" element={<Live />}>
-          <Route index element={<EventView />} />
-          <Route path="event-view" element={<EventView />} />
-          <Route path="*" element={<Navigate to="event-view" replace />} />
-        </Route>
-      </Routes>
-
-      {background && (
-        <>
-          <Routes>
-            <Route path="faq" element={<FAQ />} />
-          </Routes>
-          <Routes>
-            <Route path="privacy-policy" element={<PrivacyPolicy />} />
-          </Routes>
-        </>
-      )}
-    </div>
+        {background && (
+          <>
+            <Routes>
+              <Route path="faq" element={<FAQ />} />
+            </Routes>
+            <Routes>
+              <Route path="privacy-policy" element={<PrivacyPolicy />} />
+            </Routes>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
