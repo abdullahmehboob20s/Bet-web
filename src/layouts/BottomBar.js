07446d0055d38f1a454384f7ab83fea51b00331b
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-14 12:47:51 +0500

    passing layout in portal

--- src/layouts/BottomBar.js
+++ src/layouts/BottomBar.js
@@ -1,9 +1,14 @@
 import BottomBarLink from "components/BottomBarLink";
 import { FiMenu } from "react-icons/fi";
+import { useDispatch } from "react-redux";
+import { toggleBottomMenuModalOpen } from "redux/bottomMenuModalState";
+import ReactDOM from "react-dom";
 
 function BottomBar() {
-  return (
-    <div className="fix-lag fixed left-0 bg-[rgba(0,12,36,.9)] border-t-1px border-white-2 bottom-0 w-full h-mobile-header-main-section-height z-[100] backdrop-blur-[3px] flex items-center">
+  const dispatch = useDispatch();
+
+  return ReactDOM.createPortal(
+    <div className="fix-lag fixed left-0 bg-[rgba(0,12,36,.9)] border-t-1px border-white-2 bottom-0 w-full h-mobile-header-main-section-height z-[400] backdrop-blur-[3px] flex items-center">
       <BottomBarLink title="Live" />
       <BottomBarLink title="Sports" />
       <BottomBarLink title="Casino" />
@@ -11,8 +16,10 @@ function BottomBar() {
         isButton={true}
         title="Menu"
         icon={<FiMenu className="text-2xl" style={{ filter: "invert(.5)" }} />}
+        onClick={() => dispatch(toggleBottomMenuModalOpen(true))}
       />
-    </div>
+    </div>,
+    document.getElementById("modals")
   );
 }
 
